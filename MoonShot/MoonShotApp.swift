899774fed7182173f69fa609a6a0ed005b002e9d
Che <vee.langat@gmail.com>
2021-03-07 18:05:40 +0400

    initial commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Chepkorir Lang'at on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
